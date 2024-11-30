cc7d0634b4c0c3a0266d2db3ee45df72675a7d8a
lordgr8t <lordgr8t@gmail.com>
2024-12-01 02:28:38 +0300

    postman q work

--- api/v1/user.php
+++ api/v1/user.php
@@ -1,6 +1,12 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+
+session_start();
+
 header('Content-Type: application/json');
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: x-api-key, Content-Type");
 
 include('../vars.php');
 
@@ -169,7 +175,9 @@ switch ($requestMethod) {
     }
     break;
 
-
+    case 'OPTIONS':
+        http_response_code(200);
+        exit();
 
 
     default:
